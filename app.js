4a6b6050e3fc5a2a53b98faa1beabfd176d0762f
Barel Shraga <barel123133@gmail.com>
2022-07-26 11:32:53 +0300

    bcrypt

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
-
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+////////////////////////////////////////////
 // Express setup
-
+////////////////////////////////////////////
 const app = express();
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 
+////////////////////////////////////////////
 // mongoose setup
-
+////////////////////////////////////////////
 mongoose.connect('mongodb://localhost:27017/userDB');
 
 const userSchema = new mongoose.Schema({
@@ -22,12 +23,11 @@ const userSchema = new mongoose.Schema({
 	password: String,
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
-
 const User = new mongoose.model('User', userSchema);
 
+////////////////////////////////////////////
 // Routes
-
+////////////////////////////////////////////
 app.get('/', (req, res) => {
 	res.render('home');
 });
@@ -39,18 +39,22 @@ app.route('/login')
 	.post((req, res) => {
 		const { username, password } = req.body;
 
-		User.findOne({ username: username }, (err, found) => {
+		User.findOne({ username: username }, (err, foundUser) => {
 			if (err) {
 				console.log(err);
 			} else {
-				if (found) {
-					if (found.password === password) {
-						res.render('secrets');
-					} else {
-						res.send('BAD PASSWORD');
-					}
+				if (foundUser) {
+					bcrypt.compare(password, foundUser.password, (err, result) => {
+						if (err) {
+							console.log(err);
+						} else if (result) {
+							res.render('secrets');
+						} else {
+							res.send('Bad password.');
+						}
+					});
 				} else {
-					res.send('BAD USER');
+					res.send('Unable to find user.');
 				}
 			}
 		});
@@ -63,14 +67,29 @@ app.route('/register')
 	.post((req, res) => {
 		const { username, password } = req.body;
 
-		new User({ email: username, password: password }).save((err) => {
-			if (err) console.log(err);
-			else res.render('secrets');
+		bcrypt.hash(password, saltRounds, (err, hash) => {
+			if (err) {
+				console.log(err);
+			} else {
+				User.findOne({ email: username }, (err, foundUser) => {
+					if (err) {
+						console.log(err);
+					} else if (foundUser) {
+						res.send('User already exists!');
+					} else {
+						new User({ email: username, password: hash }).save((err) => {
+							if (err) console.log(err);
+							else res.render('secrets');
+						});
+					}
+				});
+			}
 		});
 	});
 
+////////////////////////////////////////////
 // App Listen
-
+////////////////////////////////////////////
 app.listen(process.env.PORT || '3000', () => {
 	console.log(`Server started on port ${process.env.PORT || 3000}`);
 });
