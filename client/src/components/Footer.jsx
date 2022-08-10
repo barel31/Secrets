45fa039e271c7c93e6704fbaa0f91d9e51c5d6b2
Barel Shraga <barel123133@gmail.com>
2022-08-10 11:59:01 +0300

    Footer update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -7,12 +7,11 @@ export default function Footer() {
 			<footer>
 				<p>
 					Made with 🤍 and passion by{' '}
-					<a href="https://github.com/barel31/" target="_blank" rel="noreferrer" aria-describedby='sad'>
+					<a href="https://github.com/barel31/" target="_blank" rel="noreferrer" aria-describedby="sad">
 						barel31
 					</a>
-				</p>
-				<p>
-					Would love to here from you: <Link to={'/feedback'}>Feedback</Link>
+					<br />
+					I would love to here from you: <Link to={'/feedback'}>Feedback</Link>
 				</p>
 			</footer>
 		</div>
